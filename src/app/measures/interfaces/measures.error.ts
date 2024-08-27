b7c0fe8cc732c0a9e5ba5bfd41caba0287ab509f
lohanvictor <lohan.victor@hotmail.com>
2024-08-27 16:33:51 -0400

    feat(/upload): verify errors in request body

---
+++ src/app/measures/interfaces/measures.error.ts
@@ -0,0 +1,4 @@
+export enum MeasuresRequestErrors {
+  INVALID_DATA = "INVALID_DATA",
+  DOUBLE_REPORT = "DOUBLE_REPORT",
+}
--- src/app/measures/measures.controller.ts
+++ src/app/measures/measures.controller.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
 import { UploadInputDTO } from "./dto/upload.dto";
+import { MeasuresUtils } from "./measures.utils";
+import { MeasuresRequestErrors } from "./interfaces/measures.error";
 
 export class MeasuresController {
   static async upload(req: Request<{}, {}, UploadInputDTO>, res: Response) {
     // verificar o body da requisição
-    
+    const errors = MeasuresUtils.getErrorsInUpload(req.body);
+    if (errors.length > 0) {
+      return res.status(400).json({
+        error_code: MeasuresRequestErrors.INVALID_DATA,
+        error_description: errors,
+      });
+    }
+
     // verificar se a leitura já foi feita no mês atual
 
     // realizar operação de upload
