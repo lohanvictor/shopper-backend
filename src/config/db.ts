94f9e454eef25a2c94b98039f4fda87769320f4a
lohanvictor <lohan.victor@hotmail.com>
2024-08-29 01:28:41 -0400

    feat: add database config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,14 @@
 import { DataSource } from "typeorm";
+import { MeasureEntity } from "../app/measures/models/measure.entity";
 
 export const db = new DataSource({
   type: "postgres",
-  host: process.env.POSTGRES_HOST || "localhost",
+  host: process.env.POSTGRES_HOST || "shopper_db",
   port: +(process.env.POSTGRES_PORT || 5432),
-  username: process.env.POSTGRES_USER || "lohan",
-  password: process.env.POSTGRES_PASSWORD || "admin",
-  database: "measures",
-  synchronize: false,
-  logging: true,
-  entities: [],
+  username: process.env.POSTGRES_USER || "postgres",
+  password: process.env.POSTGRES_PASSWORD || "postgres",
+  database: "postgres",
+  synchronize: true,
+  logging: false,
+  entities: [MeasureEntity],
 });
