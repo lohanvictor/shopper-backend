7677a4e0e38b2f456fce8b4d0c819082e02dd3cc
lohanvictor <lohan.victor@hotmail.com>
2024-08-30 11:23:48 -0400

    refactor: change db config properties

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,10 +3,10 @@ import { MeasureEntity } from "../app/measures/models/measure.entity";
 
 export const db = new DataSource({
   type: "postgres",
-  host: process.env.POSTGRES_HOST || "shopper_db",
-  port: +(process.env.POSTGRES_PORT || 5432),
-  username: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "postgres",
+  host: "shopper_db",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
   database: "postgres",
   synchronize: true,
   logging: false,
