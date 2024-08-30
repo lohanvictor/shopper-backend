68c8aae0cc04149bc6921eb78b5f07ef80a77370
lohanvictor <lohan.victor@hotmail.com>
2024-08-30 13:21:16 -0400

    docs: files of gemini service

--- src/services/gemini/gemini.service.ts
+++ src/services/gemini/gemini.service.ts
@@ -2,6 +2,14 @@ import { unlink, writeFile } from "fs/promises";
 import { geminiFileManager, geminiModel } from "../../config/gemini";
 
 export class GeminiService {
+  /**
+   * Retorna o valor de medição da imagem.
+   * 
+   * @param data - A imagem codificada em Base64.
+   * @param mimeType - O MIME type da imagem.
+   * @param measureType - O tipo de medida da imagem (water/gas).
+   * @returns Retorna um inteiro representando o valor identificado na imagem. Caso não identifique, retorna -1.
+   */
   static async getMeasure(
     data: string,
     mimeType: string,
@@ -28,6 +36,13 @@ export class GeminiService {
     }
   }
 
+  /**
+   * Armazena uma iamgem no gerenciador de arquivos do Gemini.
+   * 
+   * @param name - O nome da imagem.
+   * @param imageBase64 - A imagem codificada em Base64.
+   * @returns Um URI da imagem no  gerenciador de arquivos do Gemini.
+   */
   static async uploadImage(name: string, imageBase64: string): Promise<string> {
     const path = "./out.png";
     await writeFile(path, imageBase64, "base64");
